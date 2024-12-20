84c691fa3a4f8eae4deaba015f67ae79829c26e5
minagishl <mvxd568.co@icloud.com>
2024-12-20 17:51:55 +0900

    Automatic base switching

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+export default defineConfig(({ command }) => {
+  return {
+    base: command === "build" ? "/chroma/" : "/",
+    plugins: [react(), tsconfigPaths()],
+  };
 });
